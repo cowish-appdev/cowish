0ff741d90e585c9859a50ac9a89ee19421155da6
Fin <Fin0725.1@gmail.com>
2025-05-08 15:14:22 +0700

    pages

---
+++ cowish-app/app/(tabs)/profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ProfilePage() {
+  return (
+    <View>
+      <Text>My Profile / Personal Wishlist</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  white: {
+    color: "white",
+  },
+});
