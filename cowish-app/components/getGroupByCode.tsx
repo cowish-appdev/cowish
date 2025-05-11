4c62a845e82fd45cbd501fabdbd5b4432fedca01
jsllp00 <lingling.prapaisuwon@gmail.com>
2025-05-11 21:59:15 +0700

    added login and edit profile

---
+++ cowish-app/components/getGroupByCode.tsx
@@ -0,0 +1,19 @@
+
+export default function getGroupByCode(code:string,setGroup:Function){
+    const getGroupByCode = async(code:string,setGroup:Function)=>{
+        try {
+            const response = await fetch(`http://127.0.0.1:5000/groups/code/${code}`)
+            console.log(response.ok)
+            if(!response.ok){
+                throw new Error('Network responser was not ok');
+            }
+            const data = await response.json();
+            console.log(data)
+            setGroup(data);
+        }catch(error){
+            console.error('Fetch group failed: ',error);
+            throw error;
+        }
+    }
+    getGroupByCode(code,setGroup)
+}
